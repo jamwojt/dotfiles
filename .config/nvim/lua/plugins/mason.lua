3e96535848852a816046d9c0cc09392040bd904a
jamwojt <>
2025-11-07 14:32:00 +0100

    update lsp

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,98 +1,103 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "ruff" },
-			})
-		end,
-		{
-			"neovim/nvim-lspconfig",
-			dependencies = {
-				"saghen/blink.cmp",
-			},
-			lazy = false,
-			init = function()
-				local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
-				local capabilities = require("blink.cmp").get_lsp_capabilities()
-				lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright", "ruff" },
+            })
+        end,
+        {
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                "saghen/blink.cmp",
+            },
+            lazy = false,
+            init = function()
+                local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
+                local capabilities = require("blink.cmp").get_lsp_capabilities()
+                lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
 
-				local lspConfig = require("lspconfig")
+                local lspConfig = require("lspconfig")
 
-				lspConfig.lua_ls.setup({ capabilities = capabilities })
+                lspConfig.lua_ls.setup({ capabilities = capabilities })
 
-				lspConfig.pyright.setup({
-					capabilities = capabilities,
-				})
+                lspConfig.pyright.setup({
+                    capabilities = capabilities,
+                })
 
-				lspConfig.ruff.setup({
-					-- capabilities = capabilities,
-					init_options = {
-						settings = {
-							lineLength = 88,
-							fixAll = true,
-							lint = {
-								enable = true,
-								extendSelect = {
-									"ANN001",
-									"ANN2",
-									"SIM",
-									"ARG",
-									"NPY",
-									"PD",
-									"PERF",
-									"E",
-									"W",
-									"D",
-									"F",
-									"RUF",
-									"TRY",
-								},
-								ignore = {
-									"D100",
-									"D4",
-								},
-							},
-							format = {
-								quouteStyle = "double",
-								docstringCodeFormat = true,
-								organizeImports = true,
-							},
-						},
-					},
-				})
+                lspConfig.omnisharp.setup({
+                    cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/OmniSharp"), "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+                    capabilities = capabilities,
+                })
 
-				lspConfig.gopls.setup({
-				    capabilities = capabilities,
-				    settings = {
-				        gopls = {
-				            analyses = {
-				                unusedparams = true,
-				            },
-				            staticcheck = true,
-				            gofumpt = true,
-				        },
-				    },
-				})
+                lspConfig.ruff.setup({
+                    -- capabilities = capabilities,
+                    init_options = {
+                        settings = {
+                            lineLength = 88,
+                            fixAll = true,
+                            lint = {
+                                enable = true,
+                                extendSelect = {
+                                    "ANN001",
+                                    "ANN2",
+                                    "SIM",
+                                    "ARG",
+                                    "NPY",
+                                    "PD",
+                                    "PERF",
+                                    "E",
+                                    "W",
+                                    "D",
+                                    "F",
+                                    "RUF",
+                                    "TRY",
+                                },
+                                ignore = {
+                                    "D100",
+                                    "D4",
+                                },
+                            },
+                            format = {
+                                quouteStyle = "double",
+                                docstringCodeFormat = true,
+                                organizeImports = true,
+                            },
+                        },
+                    },
+                })
 
-				lspConfig.tinymist.setup({
-					capabilities = capabilities,
-					settings = {
-						formatterMode = "typstyle",
-						exportPdf = "never",
-					},
-				})
+                lspConfig.gopls.setup({
+                    capabilities = capabilities,
+                    settings = {
+                        gopls = {
+                            analyses = {
+                                unusedparams = true,
+                            },
+                            staticcheck = true,
+                            gofumpt = true,
+                        },
+                    },
+                })
 
-				lspConfig.rust_analyzer.setup({
-					capabilities = capabilities,
-				})
-			end,
-		},
-	},
+                lspConfig.tinymist.setup({
+                    capabilities = capabilities,
+                    settings = {
+                        formatterMode = "typstyle",
+                        exportPdf = "never",
+                    },
+                })
+
+                lspConfig.rust_analyzer.setup({
+                    capabilities = capabilities,
+                })
+            end,
+        },
+    },
 }
